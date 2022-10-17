29671124a1f5301f49b7972d5f5ec38a999118df
cacheup <dcastelan613@gmail.com>
2022-10-17 18:03:30 -0400

    Build functions to let players mark the gameboard

--- javascript.js
+++ javascript.js
@@ -11,17 +11,44 @@ const gameBoard = (() => {
       spaces[i].textContent = _board[i];
     }
   }
+  const markBoard = (space, marker) => {
+    if(space > -1 && space < 9) {
+      if(_board[space] == ""){
+         _board[space] = marker;
+      }
+    }
+  }
   return {
     resetBoard,
-    displayBoard
+    displayBoard,
+    markBoard
   };
 })();
 
 const playerFactory = (name, marker) => {
-  return { name, marker };
+  const markBoard = (space) => {
+    gameBoard.markBoard(space, marker);
+  }
+  return {
+    name,
+    marker,
+    markBoard
+  };
 }
 
 const game = (() => {
   const _player1 = playerFactory("P1", "x");
   const _player2 = playerFactory("P2", "o");
+  let _currentPlayer = _player1;
+  const _spaces = document.querySelectorAll('.space');
+  _spaces.forEach(space => space.addEventListener('click', () => {
+    if(space.textContent == "") {
+      _currentPlayer.markBoard(space.getAttribute('id'));
+      space.textContent = _currentPlayer.marker;
+      _currentPlayer = _currentPlayer == _player1 ? _player2 : _player1; 
+    }
+  }));
+  return {
+
+  }
 })();
