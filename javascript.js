367029a72e0e54297f3da94c88c5b1a7704ff76a
cacheup <dcastelan613@gmail.com>
2022-10-17 21:02:18 -0400

    Check for when the game is over

--- javascript.js
+++ javascript.js
@@ -18,10 +18,59 @@ const gameBoard = (() => {
       }
     }
   }
+  const isThreeinRow = (space, marker) => {
+    switch (space) {
+      case 0:
+        if(_board[1] == marker && _board[2] == marker) {return true;}
+        if(_board[3] == marker && _board[6] == marker) {return true;}
+        if(_board[4] == marker && _board[8] == marker) {return true;}
+        return false;
+      case 1:
+        if(_board[0] == marker && _board[2] == marker) {return true;}
+        if(_board[4] == marker && _board[7] == marker) {return true;}
+        return false;
+      case 2:
+        if(_board[0] == marker && _board[1] == marker) {return true;}
+        if(_board[4] == marker && _board[6] == marker) {return true;}
+        if(_board[5] == marker && _board[8] == marker) {return true;}
+        return false;
+      case 3:
+        if(_board[0] == marker && _board[6] == marker) {return true;}
+        if(_board[4] == marker && _board[5] == marker) {return true;}
+        return false;
+      case 4:
+        if(_board[0] == marker && _board[8] == marker) {return true;}
+        if(_board[1] == marker && _board[7] == marker) {return true;}
+        if(_board[2] == marker && _board[6] == marker) {return true;}
+        if(_board[3] == marker && _board[5] == marker) {return true;}
+        return false;
+      case 5:
+        if(_board[2] == marker && _board[8] == marker) {return true;}
+        if(_board[3] == marker && _board[4] == marker) {return true;}
+        return false;
+      case 6:
+        if(_board[0] == marker && _board[3] == marker) {return true;}
+        if(_board[2] == marker && _board[4] == marker) {return true;}
+        if(_board[7] == marker && _board[8] == marker) {return true;}
+        return false;
+      case 7:
+        if(_board[1] == marker && _board[4] == marker) {return true;}
+        if(_board[6] == marker && _board[8] == marker) {return true;}
+        return false;
+      case 8:
+        if(_board[0] == marker && _board[4] == marker) {return true;}
+        if(_board[2] == marker && _board[5] == marker) {return true;}
+        if(_board[6] == marker && _board[7] == marker) {return true;}
+        return false;
+      default:
+        return false;
+    }
+  }
   return {
     resetBoard,
     displayBoard,
-    markBoard
+    markBoard,
+    isThreeinRow
   };
 })();
 
@@ -40,12 +89,22 @@ const game = (() => {
   const _player1 = playerFactory("P1", "x");
   const _player2 = playerFactory("P2", "o");
   let _currentPlayer = _player1;
+  let _count = 1;
   const _spaces = document.querySelectorAll('.space');
   _spaces.forEach(space => space.addEventListener('click', () => {
     if(space.textContent == "") {
-      _currentPlayer.markBoard(space.getAttribute('id'));
+      _currentPlayer.markBoard(+space.getAttribute('id'));
       space.textContent = _currentPlayer.marker;
-      _currentPlayer = _currentPlayer == _player1 ? _player2 : _player1; 
+      if(gameBoard.isThreeinRow(+space.getAttribute('id'), _currentPlayer.marker)) {
+        console.log(`${_currentPlayer.name} won!`);
+        return;
+      }
+      if(_count == 9) {
+        console.log('tie');
+        return;
+      }
+      _currentPlayer = _currentPlayer == _player1 ? _player2 : _player1;
+      _count++;
     }
   }));
   return {
